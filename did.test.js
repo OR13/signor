ac4f8cfdf45e18842e8b5db77c5a07818646d483
Orie Steele <orie@transmute.industries>
2022-04-16 17:34:56 -0500

    chore:test

--- did.test.js
+++ did.test.js
@@ -1,5 +1,5 @@
 const did = require('.')
 
 it('can require did document', () => {
-    expect(did.id).toBe('did:key:zDnaegNT8gHZ9ck56BUUuATyiFBQQPFUNFEexw1cXhiu3BfSG')
+    expect(did.id).toBe('did:web:or13.github.io:signor')
 })
